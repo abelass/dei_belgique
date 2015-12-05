9a21dc0c5a48c34e368978910ace5d9086d74795
Rainer <rainer@websimple.be>
2015-12-05 10:42:49 +0100

    import initial

---
+++ dei_belgique_pipelines.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Utilisations de pipelines par DEI Belgique
+ *
+ * @plugin     DEI Belgique
+ * @copyright  2015
+ * @author     Rainer
+ * @licence    GNU/GPL
+ * @package    SPIP\Dei_belgique\Pipelines
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+	
+
+/*
+ * Un fichier de pipelines permet de regrouper
+ * les fonctions de branchement de votre plugin
+ * sur des pipelines existants.
+ */
+
+
+
+
+?>
---
+++ lang/paquet-dei_belgique_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// D
+	'dei_belgique_description' => 'Squelette DEI Belgique',
+	'dei_belgique_nom' => 'DEI Belgique',
+	'dei_belgique_slogan' => 'Squelette DEI Belgique',
+);
+
+?>
