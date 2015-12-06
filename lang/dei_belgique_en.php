ca9e3b438011ad6100616bbdab933c1c99d99355
Rainer Müller <rainer@websimple.be>
2015-12-06 18:44:17 +0100

    footer

--- lang/dei_belgique_en.php
+++ lang/dei_belgique_en.php
@@ -4,19 +4,21 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'adresse' => 'Rue du Marché aux Poulets 30<br/>1000 Bruxelles',
 
 	// D
 	'dei_belgique_titre' => 'DEI Belgique',
-	
+
 	// I
 	'inscription_newsletter' => 'Subscribe to the DCI Newsletter',
-	
+
 	// L
 	'latest_news' => 'Latest news',
-	
+
 	// M
 	'mouvement_modial_enfants' => 'Le mouvement mondial pour les droits de l\'enfant',
-	
+
 );
 
 ?>
--- lang/dei_belgique_fr.php
+++ lang/dei_belgique_fr.php
@@ -4,19 +4,21 @@
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
+	// A
+	'adresse' => 'Rue du Marché aux Poulets 30<br/>1000 Bruxelles',
 
 	// D
 	'dei_belgique_titre' => 'DCI Belgium',
-	
+
 	// I
 	'inscription_newsletter' => 'Inscrivez-vous à la newsletter DEI',
-	
+
 	// L
 	'latest_news' => 'Dernières Nouvelles',
-	
+
 	// M
 	'mouvement_modial_enfants' => 'The worldwide movement for children\'s rights',
-	
+
 );
 
 ?>
