3767d663db3c74c283cdcfb8aed384608596d7d5
Rainer <rainer@websimple.be>
2016-01-09 10:30:48 +0100

    améliorer le filtre google like

--- dei_belgique_fonctions.php
+++ dei_belgique_fonctions.php
@@ -20,16 +20,27 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
  * 
  */
 
+/*
+ * Filtre pour afficher mettre en évidence le terme recherché
+ * @param string $string le texte à parser
+ * @param array $options 
+ * 	resume : si le terme n'est pas trouvé, n'affiche rien.
+ * 	wrapper : entourer le résultat d'un string déterminé
+ * 	taille : nombre characterers avant et après le terme
+ * @return string
+ */
+function recherche_avancee_google_like($string, $options=array()){
+	
+	$resume = isset($options['resume']) ? $options['resume'] : ($options['resume'] != 'non' ? $string : '');
+	$wrapper = isset($options['wrapper']) ? '<i class="rsusp">[...]</i>' : '';
+	$taille = isset($options['taille']) ? $options['taille'] : 55;
 
-function recherche_avancee_google_like($string, $resume='')
-
-{	// Convertir en texte brut sans accent
-
+	// Convertir en texte brut sans accent
 	$string = textebrut($string);
 
 	$string = translitteration($string);
 
-	$rech = translitteration($_GET['recherche']);
+	$rech = translitteration(_request('recherche'));
 
 	// Supprimer les caracteres qui m...
 
@@ -47,44 +58,43 @@ function recherche_avancee_google_like($string, $resume='')
 
 	// $cc = ceil(55 / $num);
 
-	$cc=55;
+	 $cc=$taille;
+
 
 	for ($i = 0; $i < $num; $i++) 
 
 	{
 		$tab[$i] = preg_split("/\b($qt[$i])/i",$string,2, PREG_SPLIT_DELIM_CAPTURE);
 
-		if(count($tab[$i])>1)
+		if (count($tab[$i])>1) {
+			// Chaine avant
+			if ($wrapper) {
+				$avant = substr($tab[$i][0],-$cc,$cc);
+				$mots = split(" ",$avant,2);
+				if (count($mots)>1) $avant = $mots[1];
+			}
+			else $avant = $tab[$i][0];
 
-		{	// Chaine avant
 
-			$avant = substr($tab[$i][0],-$cc,$cc);
-
-			$mots = split(" ",$avant,2);
-
-			if (count($mots)>1) $avant = $mots[1];
 
 			// Chaine apres
-
-			$apres = substr($tab[$i][2],0,$cc);
-
+			if ($wrapper) $apres = substr($tab[$i][2],0,$cc);
+			else $apres = $tab[$i][2];
 			$apres = preg_replace('@(.+)\s\S+@s', '\1', $apres);
 
 			// Concatener
-
-			if ($string_re=='') $string_re = "<i class=rsusp>[...]</i>";
-
-			$string_re .= " $avant<span class=spip_surligne>".$tab[$i][1]."</span>$apres <i class=rsusp>[...]</i> ";
-
+			if ($string_re=='') {
+				$string_re = $wrapper;
+			}
+			$string_re .= " $avant<span class=spip_surligne>".$tab[$i][1]."</span>$apres $wrapper";
 		}
-
 	}
 
 	// Si rien trouve : renvoyer les premiers mots en resume
 
-	if ($resume!='' && $string_re=='')
-
-	{	$mots = split(" ",$string,40);
+	if ($resume != '' AND $string_re == ''){
+		echo 'ok';
+		$mots = split(" ",$string,40);
 
 		for ($i = 0; $i < count($mots)-1; $i++)
 
