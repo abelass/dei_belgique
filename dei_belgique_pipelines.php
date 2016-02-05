413103d5eaaa55807869c0757f63503a0215f26f
Rainer <rainer@websimple.be>
2016-02-05 11:51:16 +0100

    ajout de justifier et souligner à la barre outils

--- dei_belgique_pipelines.php
+++ dei_belgique_pipelines.php
@@ -8,17 +8,87 @@
  * @licence    GNU/GPL
  * @package    SPIP\Dei_belgique\Pipelines
  */
+if(!defined('_ECRIRE_INC_VERSION'))
+	return;
 
-if (!defined('_ECRIRE_INC_VERSION')) return;
-	
-
-/*
- * Un fichier de pipelines permet de regrouper
- * les fonctions de branchement de votre plugin
- * sur des pipelines existants.
+/**
+ * Ajout des boutons à la barre d'edition
+ *
+ * @pipeline porte_plume_barre_outils_pre_charger
+ * @param  objet $barres La définition de la barre
+ * @return objet La définition de la barre
  */
+function dei_belgique_porte_plume_barre_pre_charger($barres) {
+	
+	$barre=&$barres['edition'];
+	
+	$barre->ajouterApres(
+		'alignerdroite',array(
+			// justifier <just>
+			"id" => 'justifie',
+			"name" => _T('dei_belgique:barre_outil_justifie'),
+			"className"=>'outil_justifie',
+			"openWith"=>"<justifie>",
+			"closeWith"=>"</justifie>",
+			"display"=>true,
+		)
+	);
+	
+	$barre->ajouterAvant(
+		'petitescapitales',array(
+			// souligner <souligner>
+			"id" => 'souligne',
+			"name" => _T('dei_belgique:barre_outil_souligne'),
+			"className"=>'outil_souligne',
+			"openWith"=>"<souligne>",
+			"closeWith"=>"</souligne>",
+			"display"=>true,
+		)
+	);
+	
+	return $barres;
+}
 
+/**
+ * Lie les icones au boutons de la barre d'edition
+ *
+ * @pipeline porte_plume_lien_classe_vers_icone
+ * @param  array $flux Les icones de la barre
+ * @return array $flux Les icones de la barre
+ */
+function dei_belgique_porte_plume_lien_classe_vers_icone($flux) {
+	return array_merge($flux, array(	
+		'outil_justifie' => 'justifie.png',
+		'outil_souligne' => 'souligne.png',
+	));
+}
 
+/**
+ * Insertion dans le pipeline post_propre (SPIP)
+ * 
+ * Remplacement de caracteres apres le passage de propre
+ * 
+ * @param $texte string
+ * 	Le texte a modifier
+ * @return $texte string
+ */
+function dei_belgique_pre_propre($texte) {
 
+	$search = array(
+		'<justifie>',
+		'</justifie>',
+		'<souligne>',
+		'</souligne>'
+	);
+	$replace = array(
+		"<p class='justifie'>",
+		'',
+		'<span class="souligne">',
+		'</span>'
+	);
+		
+	$texte = str_replace($search, $replace, $texte);
 
-?>
+	return $texte;
+}
+
